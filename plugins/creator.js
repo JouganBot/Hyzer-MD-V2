a92afea40124ed4b27e3c4aa1cce2ee2fe5d7871
JouganBot <88159546+JouganBot@users.noreply.github.com>
2022-04-10 14:04:16 +0700

    update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -8,7 +8,7 @@ const q = {
 	},
 	"message": {
 		"contactMessage": {
-			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285643354073:+62 856-4335-4073\nEND:VCARD"
+			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6288276765449:+62 882-7676-5449\nEND:VCARD"
 		}
 	},
 	"participant": "0@s.whatsapp.net"
